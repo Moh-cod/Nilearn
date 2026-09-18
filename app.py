ad374e7dfaad7317a8cf3795144da41a620abcb7
Moh-cod <ymohmmed34@gmail.com>
2026-09-18 21:10:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def resources():
     connection.close()
 
     return render_template("resources.html",
-                           resources=resources, subjects=subjects, subject_names=subject_names, resource_type_name)
+                           resources=resources, subjects=subjects, subject_names=subject_names, resource_type_name=resource_type_names)
 
 
 
